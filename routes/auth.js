70e3453be54d6d13a5053234d4d21a6e03c44d79
minhasshaa11 <minhasshaa@gmail.com>
2025-10-16 03:52:29 +0500

    Fix capitalization of 'const' in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-Const express = require('express');
+const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/User');
